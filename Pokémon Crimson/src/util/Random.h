1a50bfe54ec8f3c80bc0e0239a11fdcf8682309b
MaxBuilder <mripadspace@hotmail.fr>
2020-08-17 21:20:51 +0200

    Added number generator and textbox ressources

--- Pokémon Crimson/src/util/Random.h
+++ Pokémon Crimson/src/util/Random.h
@@ -1,2 +1,33 @@
 #pragma once
 
+#include<random>
+#include<ctime>
+
+class Random final {
+public:
+	Random() = delete;
+
+	static int Generate(int p_min, int p_max);
+	static int Generate(float p_min, float p_max);
+	static bool TryPercentage(float p_percentage);
+
+private:
+	static std::default_random_engine generator;
+};
+
+std::default_random_engine Random::generator = std::default_random_engine(static_cast<unsigned int>(time(0)));
+
+int Random::Generate(int p_min, int p_max) {
+	std::uniform_int_distribution<int> distribution(p_min, p_max);
+	return distribution(generator);
+}
+
+int Random::Generate(float p_min, float p_max) {
+	std::uniform_real_distribution<float> distribution(p_min, p_max);
+	return distribution(generator);
+}
+
+bool Random::TryPercentage(float p_percentage) {
+	std::uniform_real_distribution<float> distribution(0.0f, 100.0f);
+	return distribution(generator);
+}
